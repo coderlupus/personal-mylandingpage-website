f468dd831d71802caa0548d36e9c8398bcf180c6
Nathan L. <97923367+coderlupus@users.noreply.github.com>
2026-01-05 03:35:49 -0300

    feat: Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -65,7 +65,7 @@ const Hero = () => {
             style={{ animationDelay: "0.5s" }}
             className="section-subtitle mt-3 sm:mt-6 mb-4 sm:mb-8 leading-relaxed opacity-0 animate-fade-in text-gray-950 font-normal text-base sm:text-lg text-center mx-auto max-w-2xl"
           >
-            sou Desenvolvedor e empreendedor. tenho o foco em entender o desafio do cliente para entregar soluções digitais personalizadas!
+            Sou desenvolvedor e empreendedor. tenho o foco em entender o desafio do cliente para entregar soluções digitais personalizadas!
           </p>
 
           <div
